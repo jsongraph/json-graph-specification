f754b6098f8cc0df4ee7ae6af197abc591791a4f
Anthony Bargnesi <abargnesi@gmail.com>
2014-09-02 12:35:36 -0400

    corrected BEL schema, details follow
    - moved evidence inside definitions section
    - moved edge inside graph/properties
    - fixed evidence JSON pointer in edge metadata
    - fixed citation/type string enumeration
    - added BEL test to validate child schema and an example BEL network

---
+++ .build/test/test-bel.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'json-schema'
+require 'pathname'
+require 'pp'
+require 'minitest/autorun'
+require 'minitest/unit'
+
+class ExtTest < Minitest::Test
+
+  def test_validate_schema
+    root = Pathname(File.dirname(File.expand_path __FILE__)) + '..' + '..' + 'child-schemas'
+    schema = JSON.parse File.read(root + 'bel-json-graph.schema.json')
+    errors = JSON::Validator.fully_validate_schema(schema)
+
+    assert errors.empty?, errors.join("\n")
+
+    root = Pathname(File.dirname(File.expand_path __FILE__)) + '..' + '..'
+    data = JSON.parse File.open(root + 'examples' + 'test.network.json', 'r:UTF-8', &:read)
+    errors = JSON::Validator.fully_validate(schema, data, :insert_defaults => true, :errors_as_objects => true)
+
+    if ENV['JGS_VERBOSE']
+      puts "\n\nValidated JSON for example: examples/test.network.json"
+      pp data
+    end
+
+    assert errors.empty?, errors.join("\n")
+  end
+end
+# vim: ts=2 sts=2 sw=2
