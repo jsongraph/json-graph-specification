9c0c45acb9804c864b1a978bb4ba87fa7e4b315d
ArSn <koljaz@web.de>
2020-11-18 00:29:02 +0100

    First approach to tests via python.

---
+++ test/test-examples.py
@@ -0,0 +1,14 @@
+from jsonschema import FormatChecker, TypeChecker, exceptions, validators
+import unittest
+
+# Test examples against JGF
+class SchemaTest(unittest.TestCase):
+
+
+    def test_schema(self):
+
+		# todo: add some reasonable test processing here
+        self.assertEqual(
+            'fail',
+            'fail',
+        )
