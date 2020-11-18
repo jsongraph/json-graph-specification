41a18c5328065648d802d13396d346c47d96b164
ArSn <koljaz@web.de>
2020-11-18 23:30:48 +0100

    Made schema example tests actually run. Removed obsolete schema test since its already covered implicitly.

--- test/test-examples.py
+++ test/test-examples.py
@@ -1,14 +1,21 @@
-from jsonschema import FormatChecker, TypeChecker, exceptions, validators
+from jsonschema import validate
+import glob
 import unittest
+import json
 
 # Test examples against JGF
+# JGF schema against the meta schema is validated implicitly by jsonschema
 class SchemaTest(unittest.TestCase):
 
+	def test_schema(self):
 
-    def test_schema(self):
+		with open('json-graph-schema_v2.json') as file:
+			schema = json.load(file)
 
-		# todo: add some reasonable test processing here
-        self.assertEqual(
-            'fail',
-            'fail',
-        )
+		for filename in glob.glob('examples/*.json'):
+
+			with open('examples/car_graphs.json') as file:
+				instance = json.load(file)
+
+			validate(instance=instance, schema=schema)
+			print('File {} is valid against v2 schema.'.format(filename))
